3bbd4f8bbb81eed25476078a83e875bd29c84599
vlasJedi <azmodei@protonmail.com>
2022-06-25 18:41:19 +0300

    initiated

---
+++ xamarinblank/xamarinblank/Models/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace xamarinblank.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Due { get; set; }
+        public bool Completed { get; set; }
+    }
+}
